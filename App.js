29fe45bb95bb1f77d437e7b6d2318dc74415d14a
lk989 <lma.bugis@hotmail.com>
2024-03-07 00:55:10 +0300

    setup tailwind

--- App.js
+++ App.js
@@ -3,14 +3,14 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './Home';
+import Home from './Screens/Home';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Home" >
         <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
---
+++ Screens/Home.js
@@ -0,0 +1,10 @@
+import { View, Text, Image, FlatList, TouchableOpacity } from "react-native";
+
+function Home() {
+    return (
+      <View className="flex-1 items-center justify-center bg-white">
+        <Text>Hello</Text>
+      </View>
+    );
+}
+export default Home;
