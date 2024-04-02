5ad1c3cdc29ccf9f0b98ccddaa23a186c46b5ce2
Hanan <Hannony34@gmail.com>
2024-04-02 06:37:33 +0300

    add Reports Section

--- App.js
+++ App.js
@@ -7,20 +7,22 @@ import OTP from './src/screens/OTP';
 import Home from './src/screens/Home';
 import Login from './src/screens/Login';
 import Signup from './src/screens/Signup';
-import OTP from './src/screens/OTP';
 import GenerateBarcode from './src/screens/GenerateBarcode';
+import ReportsSection from './src/screens/ReportsSection';
+
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+      <Stack.Navigator initialRouteName="ReportsSection" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Signup" component={Signup} />
         <Stack.Screen name="OTP" component={OTP} />
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="GenerateBarcode" component={GenerateBarcode} />
+        <Stack.Screen name="ReportsSection" component={ReportsSection} />
       </Stack.Navigator>
     </NavigationContainer>
   );
