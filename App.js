dbc4d88cf3e754f0ac35c1adfd1310c25104ef99
Hanan <Hannony34@gmail.com>
2024-04-05 06:35:50 +0300

    change from stack to drawer

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 
 import * as React from 'react';
-import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import OTP from './src/screens/OTP';
@@ -12,7 +11,6 @@ import ObjectionDetails from './src/screens/ObjectionDetails';
 
 
 
-const Stack = createNativeStackNavigator();
 
 function App() {
   return (
