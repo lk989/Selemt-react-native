2a6eee3f8f16ac076c919aacc610a815897fdfab
alaasairafi7 <alaasairafi7@gmail.com>
2024-04-06 22:02:05 +0300

    solve error

--- App.js
+++ App.js
@@ -2,14 +2,32 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/screens/Home';
 import Login from './src/screens/Login';
-import Signup from './src/screens/Signup';
+import OTP from './src/screens/OTP';
 import GenerateBarcode from './src/screens/GenerateBarcode';
-import ObjectionDetails from './src/screens/ObjectionDetails';
 
 
 
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="OTP" component={OTP} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="GenerateBarcode" component={GenerateBarcode} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+/*
 const Drawer = createDrawerNavigator();
 
 
@@ -28,7 +46,7 @@ function App() {
           },
         }}
       >
-        <Drawer.Screen
+      <Drawer.Screen
           name="الصفحة الرئيسيه"
           component={Home}
           options={{
@@ -36,7 +54,7 @@ function App() {
               <MaterialIcons name="home" color={color} size={size} />
             ),
           }}
-        />
+        /> 
         <Drawer.Screen
           name="البلاغات"
           component={ReportsSection}
@@ -68,7 +86,7 @@ function App() {
 
       </Drawer.Navigator>
     </NavigationContainer>
-  );
-}
-
+   );
+       }
+      */
 export default App;
