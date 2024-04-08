c56e44d24283a7f43b4784235ab0d27da6757f4f
Hanan <Hannony34@gmail.com>
2024-04-08 03:32:17 +0300

    add z-50 for header viewing

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import { Icon } from 'react-native-elements'
 const Header = () => {
     let name = 'لمى بوقس';
     return ( 
-        <View className="bg-white p-4 pt-20 flex-row justify-between items-center">
+        <View className="bg-white p-4 pt-20 flex-row justify-between items-center z-50">
             <SText text='hello' params={name} classes="text-green text-2xl"/>
             <TouchableOpacity>
                 <Icon name='menu' color='#ABC7BD' size={22}/>
