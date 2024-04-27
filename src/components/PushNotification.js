b85d6e2838e53195863ccdebf92c63bbe4b5f088
Mawdah <mwdghuraybi@gmail.com>
2024-04-27 23:37:44 +0300

    update Push Notification

--- src/components/PushNotification.js
+++ src/components/PushNotification.js
@@ -1,5 +1,21 @@
-import registerNNPushToken from 'native-notify';
+import axios from 'axios';
 
-export default function TestNoti() {
-  registerNNPushToken(21001, 'wbQMKL75qxKvw4vb1Y0uFw');
-}
+
+const sendPushNotification = async () => {
+  const apiUrl = 'https://app.nativenotify.com/api/notification';
+  
+  const pushData = {
+    appId: 21001,
+    appToken: 'wbQMKL75qxKvw4vb1Y0uFw',
+    title: 'Push title here as a string',
+    body: 'Push message here as a string',
+    dateSent: '4-27-2024 8:31PM',
+  };
+  
+  try {
+    const response = await axios.post(apiUrl, pushData);
+    console.log('Push notification sent:', response.data);
+  } catch (error) {
+    console.error('Error sending push notification:', error);
+  }
+};
