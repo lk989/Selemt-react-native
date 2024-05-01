61a38d3c46421ac044d78a03754b53c82eeb1cdb
Hanan <Hannony34@gmail.com>
2024-05-01 23:38:05 +0300

    add tow functions for changing the language

--- src/components/SText.js
+++ src/components/SText.js
@@ -1,20 +1,35 @@
-import { Text } from "react-native";
-import * as Localization from 'expo-localization';
+import React from 'react';
+import { Text } from 'react-native';
 import { I18n } from 'i18n-js';
 
-const translations = {
-  ar: require('../langs/ar.json'),
-  en: require('../langs/en.json'),
-};
-const i18n = new I18n(translations);
-i18n.locale = Localization.locale;
-i18n.enableFallback = true;
+
 
 const SText = (props) => {
     let params = props.params;
     return ( 
-        <Text className={props.classes}>{i18n.t(props.text, {params})}</Text>
+        <Text className={props.classes}>{t(props.text, {params})}</Text>
     );
+}
+
+const translations = {
+  en: require('../langs/en.json'),  // Ensure your path is correct
+  ar: require('../langs/ar.json'),
+};
+
+const i18n = new I18n(translations);
+
+// Check if the current locale is Arabic or default to Arabic if not set
+
+export function t(key, params = {}) {
+  return i18n.t(key, params);
+}
+
+export function setLocale(locale) {
+  i18n.locale = locale;
 }
  
 export default SText;
+
+
+//const SText = ({ text, params = {}, style }) => {
+  //return <Text style={style}>{t(text, params)}</Text>;
