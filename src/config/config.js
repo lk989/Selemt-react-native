ba37c37d05615a8eb966707c447f18a7f6c7851e
lk989 <lma.bugis@hotmail.com>
2024-05-14 19:00:15 +0300

    change api url

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,1 @@
-// export const BASE_URL = 'http://192.168.240.149:8000/api/';
-// export const BASE_URL = 'http://127.0.0.1:8000/api/';
-export const BASE_URL = 'http://192.168.100.14:8000/api/';
+export const BASE_URL = 'https://selemt.com/api/';
