961214b55b87b691dc7fc5f2a590e33edc1c73b5
lk989 <lma.bugis@hotmail.com>
2024-05-15 16:19:15 +0300

    clean up section navigation component

--- src/navigation/SectionNavigation.js
+++ src/navigation/SectionNavigation.js
@@ -1,33 +1,36 @@
+// ? library imports 
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { getLocales } from 'expo-localization';
+
+// ? screens import
 import ObjectionsSection from '../screens/ObjectionsSection';
 import Layout from '../components/Layout';
 import ReportsSection from '../screens/ReportsSection';
-import { getLocales } from 'expo-localization';
-import { useEffect } from 'react';
 
+// ? create a Tab constant for navigation
 const Tab = createMaterialTopTabNavigator();
 
-const SectionNavigation = ({ route, navigation }) => {
-    let appLocale = getLocales()[0].languageCode;
-    let initialRouteName = route.params.initialName;
-    const reportsLabel = appLocale == 'ar' ? "البلاغات" : "Reports";
-    const objectionsLabel = appLocale == 'ar' ? "الاعتراضات" : "Objections";
-    const showPlus = true;
-    return (
-        <Layout navigation={navigation} showPlus={showPlus}>
-            <Tab.Navigator
-            tabBarOptions={{
-                activeTintColor: '#016E46', 
-                inactiveTintColor: 'gray', 
-                labelStyle: { fontSize: 12, fontWeight: 'bold' },
-                style: { borderRadius: 10 },
-                indicatorStyle: { backgroundColor: '#016E46', marginHorizontal: 5},
-              }}>
-                <Tab.Screen name={reportsLabel} component={ReportsSection} />
-                <Tab.Screen name={objectionsLabel} component={ObjectionsSection} />
-            </Tab.Navigator>
-        </Layout>
-    );
+const SectionNavigation = ({ navigation }) => {
+
+  // ? determine the label of the tab based on the chosen language
+  let appLocale = getLocales()[0].languageCode;
+  const reportsLabel = appLocale == 'ar' ? "البلاغات" : "Reports";
+  const objectionsLabel = appLocale == 'ar' ? "الاعتراضات" : "Objections";
+
+  return (
+    <Layout navigation={navigation} showPlus={true}>
+      <Tab.Navigator
+        tabBarOptions={{
+          activeTintColor: '#016E46', 
+          inactiveTintColor: 'gray', 
+          labelStyle: { fontSize: 12, fontWeight: 'bold' },
+          style: { borderRadius: 10 },
+          indicatorStyle: { backgroundColor: '#016E46', marginHorizontal: 5},
+        }}>
+          <Tab.Screen name={reportsLabel} component={ReportsSection} />
+          <Tab.Screen name={objectionsLabel} component={ObjectionsSection} />
+      </Tab.Navigator>
+    </Layout>
+  );
 }
- 
 export default SectionNavigation;
