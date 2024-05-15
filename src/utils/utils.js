dde8d1bcfa829a60290561b5927e9c80beccc397
lk989 <lma.bugis@hotmail.com>
2024-05-15 18:11:16 +0300

    clean up utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,4 @@
+// ? validate entered phone number
 export function extractCleanPhone(phone){
     let cleanPhone = phone.replace(/\D/g, "");
     if (cleanPhone.charAt(0) !== "5") {
@@ -5,9 +6,13 @@ export function extractCleanPhone(phone){
     }
     return cleanPhone;
 }
+
+// ? checks phone number's length
 export function validPhone(phone){
     return extractCleanPhone(phone).length == 9;
 }
+
+// ? accepts only digits
 export function extractCleanNumber(number){
   let cleanNumber = number.replace(/\D/g, "");
   return cleanNumber;
