b3396ee1d1a63160a0bc13ce0ed9c94e08f1038e
lk989 <lma.bugis@hotmail.com>
2024-05-15 15:46:15 +0300

    disable user from sliding back to otp from home

--- App.js
+++ App.js
@@ -7,6 +7,17 @@ import OTP from './src/screens/OTP';
 import HomeStack from './src/stacks/HomeStack';
 import Login from './src/screens/Login';
 import Signup from './src/screens/Signup';
+import Home from './src/screens/Home';
+import GenerateBarcode from './src/screens/GenerateBarcode';
+import ScanBarcode from './src/screens/ScanBarcode';
+import ObjectionDetails from './src/screens/ObjectionDetails';
+import ObjectionsSection from './src/screens/ObjectionsSection';
+import AccidentPersonalInfo from './src/screens/AccidentPersonalInfo';
+import ReportDetails from './src/screens/ReportDetails';
+import CarInformation from './src/screens/CarInformation';
+import AccidentInformation from './src/screens/AccidentInformation';
+import SectionNavigation from './src/navigation/SectionNavigation';
+import ProfileScreen from './src/screens/ProfileScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -17,7 +28,17 @@ function App() {
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Signup" component={Signup} />
         <Stack.Screen name="OTP" component={OTP} />
-        <Stack.Screen name="HomeStack" component={HomeStack} options={{ gestureEnabled: false }}/>
+        <Stack.Screen name="Home" component={Home} options={{ gestureEnabled: false }}/>
+        <Stack.Screen name="ObjectionsSection" component={ObjectionsSection} />
+        <Stack.Screen name="GenerateBarcode" component={GenerateBarcode} />
+        <Stack.Screen name="ScanBarcode" component={ScanBarcode} />
+        <Stack.Screen name="AccidentPersonalInfo" component={AccidentPersonalInfo} />
+        <Stack.Screen name="CarInformation" component={CarInformation} />
+        <Stack.Screen name="AccidentInformation" component={AccidentInformation} />
+        <Stack.Screen name="SectionNavigation" component={SectionNavigation} />
+        <Stack.Screen name="ReportDetails" component={ReportDetails} />
+        <Stack.Screen name="ObjectionDetails" component={ObjectionDetails} />
+        <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
