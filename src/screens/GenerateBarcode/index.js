ea55546cb9ec85ff2da415467244dd67c014e67a
Lama <lma.bugis@hotmail.com>
2024-05-24 16:36:27 +0300

    fix 5 interval loop problem

--- src/screens/GenerateBarcode/index.js
+++ src/screens/GenerateBarcode/index.js
@@ -13,6 +13,8 @@ import SText from "../../components/SText";
 const GenerateBarcode = ({navigation}) => {
 
     const [qrCodeImage, setQrCodeImage] = useState(null);
+    const [flag, setFlag] = useState(false);
+    const intervalId = useRef(0);
     const qrCodeIdRef = useRef(null);
 
     useEffect(() => {
@@ -25,26 +27,49 @@ const GenerateBarcode = ({navigation}) => {
             .catch(function (error) {
                 console.error("Error fetching QR code:", error.response.data.message);
             });
-            return () => clearInterval(interval);
-        }, []);
+    }, []);
 
     useEffect(() => {
-        const interval = setInterval(() => {
-            const currentQrCodeId = qrCodeIdRef.current;
-            if (currentQrCodeId) {
-                axios.post(`${BASE_URL}check-scanned-qr-code`, {barcode_id: currentQrCodeId})
-                    .then(function (response) {
-                        if (response.data != 0) {
-                            navigation.navigate('AccidentPersonalInfo', {accident_id: response.data, party: '1'});
-                        }
-                    })
-                    .catch(function (error) {
-                        console.error("Error checking boolean data:", error.response.data.message);
-                    });
-            }
-        }, 5000); 
-        return () => clearInterval(interval);
-    }, []);
+        clearInterval(intervalId.current);
+        if (!flag) {
+            intervalId.current = setInterval(getFlag, 2000);
+        }
+        return () => clearInterval(intervalId.current);
+    }, [flag]);
+    
+    async function getFlag() {
+        const currentQrCodeId = qrCodeIdRef.current;
+        const response = await fetch(`${BASE_URL}check-scanned-qr-code`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ barcode_id: currentQrCodeId })
+                });
+        const res = await response.json();
+        if (res != 0) {
+            setFlag(true);
+            navigation.navigate('AccidentPersonalInfo', {accident_id: response.data, party: '1'});
+        }
+    }
+
+    // useEffect(() => {
+    //     const interval = setInterval(() => {
+    //         if (currentQrCodeId) {
+    //             axios.post(`${BASE_URL}check-scanned-qr-code`, {barcode_id: currentQrCodeId})
+    //                 .then(function (response) {
+    //                     if (response.data != 0) {
+    //                         setFlag(true);
+    //                         navigation.navigate('AccidentPersonalInfo', {accident_id: response.data, party: '1'});
+    //                     }
+    //                 })
+    //                 .catch(function (error) {
+    //                     console.error("Error checking boolean data:", error.response.data.message);
+    //                 });
+    //         }
+    //     }, 2000); 
+    //     return () => clearInterval(interval);
+    // }, [flag]);
 
     return (
         <Layout>
